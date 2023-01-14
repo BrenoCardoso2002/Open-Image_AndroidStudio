2ea98f2654ac00117094202d00ce92d6e434ab21
Breno Cardoso <81375008+BrenoCardoso2002@users.noreply.github.com>
2023-01-14 15:24:53 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/openimage/MainActivity.kt
+++ app/src/main/java/com/example/openimage/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.openimage
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    // Cria as variaveis dos campos:
+    private lateinit var choice: Button
+    private lateinit var img: ImageView
+
+    // Cria e inicliza variaveis:
+    private val pickImage = 100
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        choice = findViewById(R.id.bt_choice_image)
+        img = findViewById(R.id.img_image_selected)
+
+        choice.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage){
+            imageUri = data?.data
+            img.setImageURI(imageUri)
+        }
     }
 }
